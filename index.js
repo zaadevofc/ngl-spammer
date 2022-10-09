0f22275c77ce76074bf7b1fe01efb9d67100520f
zaadevofc <93970726+zaadevofc@users.noreply.github.com>
2022-10-09 19:54:10 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import NGL from './src/NGL.js'
 
-const bot = new NGL('kejaazz34819', { random: true })
-bot.sent({count: 10});
+const bot = new NGL('kejaazz34819', {
+  random: true
+})
+bot.sent({
+  count: 10
+});
 
-// test
+// test anjayyyy
